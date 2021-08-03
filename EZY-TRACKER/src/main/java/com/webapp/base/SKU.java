1bc75aad052ba30677fbe08bbb07edef21cf7ee0
Geo <Geo@GEO>
2021-08-03 17:18:49 +0530

    no new changes. Just a intendation

--- EZY-TRACKER/src/main/java/com/webapp/base/SKU.java
+++ EZY-TRACKER/src/main/java/com/webapp/base/SKU.java
@@ -3,7 +3,7 @@
  */
 package com.webapp.base;
 
-import java.util.List;
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -19,67 +19,83 @@ public class SKU extends BaseClass {
 		super(driver);
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	By clickSKU = By.xpath("//p[contains(text(),'SKU')]");
 	By header = By.xpath("//h3[contains(text(),'SKU List')]");
 	By addSKU = By.xpath("//a[@title='Add SKU' and @class='btn bg-gradient-danger add-btn-header ml-2']");
-	
-	
-	By radioBtn = By.xpath("//input[@name='jApplicableFor' and @type='radio' and @value='2']");
-	
 
-	
+	By radioBtn1 = By.xpath("//input[@name='jApplicableFor' and @type='radio' and @value='1']");
+
+	By radioBtn2 = By.xpath("//input[@name='jApplicableFor' and @type='radio' and @value='2']");
+
+	/**
+	 * @return the radioBtn2
+	 */
+
 	/**
 	 * @return the clickSKU
 	 */
 	public WebElement getClickSKU() {
 		return getElement(clickSKU);
 	}
+
 	/**
 	 * @return the header
 	 */
 	public WebElement getHeader() {
 		return getElement(header);
 	}
+
 	/**
 	 * @return the addSKU
 	 */
 	public WebElement getAddSKU() {
 		return getElement(addSKU);
 	}
-	 public WebElement clickRadio(){
-		 return getElement(radioBtn);
-	 }
-		// getter
-	
 
-		public String getCompanyTitle() {
-			return getPageTitle();
+	public WebElement clickRadio1() {
+		return getElement(radioBtn1);
+	}
+
+	public WebElement clickRadio2() {
+		return getElement(radioBtn2);
+	}
+	// getter
+
+	public String getCompanyTitle() {
+		return getPageTitle();
+	}
+
+	public String getCompanyHeader() {
+		return getPageHeader(header);
+	}
+
+	public SKU SKUDetails() {
+		getClickSKU().click();
+		System.out.println("SKU is clicked");
+		getAddSKU().click();
+
+		// select radio button
+
+		if ((clickRadio1().isSelected())) {
+			clickRadio1().click();
+			System.out.println(" 1st radio button is selected:   ");
 		}
 
-		public String getCompanyHeader() {
-			return getPageHeader(header);
-		}
-		
-		public SKU SKUDetails(){
-			getClickSKU().click();
-			System.out.println("SKU is clicked");
-			getAddSKU().click();
-			
-			clickRadio().click();
-			sleep();
-			
-			return getInstance(SKU.class);
-			
-			
-			
-		}
-		
+		clickRadio2().isSelected();
+		clickRadio2().click();
+		System.out.println(" 2nd radio button is selected: ");
 
-		public SKU SKUList(){
-			getClickSKU().click();
-			sleep();
-			return getInstance(SKU.class);
-		}
+		sleep();
+
+		return getInstance(SKU.class);
+
+	}
+
+	public SKU SKUList() {
+		getClickSKU().click();
+		sleep();
+		return getInstance(SKU.class);
+	}
 
 }
--- EZY-TRACKER/src/test/java/com/testcase/tc/SKUTest.java
+++ EZY-TRACKER/src/test/java/com/testcase/tc/SKUTest.java
@@ -3,7 +3,6 @@
  */
 package com.testcase.tc;
 
-import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import com.webapp.base.Dashboard;
@@ -15,7 +14,7 @@ import com.webapp.base.SKU;
  *
  */
 public class SKUTest extends BaseTest {
-	
+
 	@Test(priority = 1, enabled = true)
 	public void newSKU() {
 		Dashboard homepage = page.getInstance(Login.class).doLogin(prop.getProperty("ACode"), prop.getProperty("UN"),
@@ -23,21 +22,17 @@ public class SKUTest extends BaseTest {
 		// it will check whether will get dashboard header
 		String headerHome = homepage.getDashboardHeader();
 		System.out.println(headerHome);
-	
-	SKU skus =  page.getInstance(SKU.class).SKUDetails();
-	sleep();
-	String com = skus.getPageTitle();
 
-	
-	//WebElement r = driver.findElements(By.xpath("//input[@name='jApplicableFor' and @type='radio' and @value='2']"))
-	System.out.println(com);
-	
-	
+		SKU skus = page.getInstance(SKU.class).SKUDetails();
+		sleep();
+		String com = skus.getPageTitle();
+
+		// WebElement r =
+		// driver.findElements(By.xpath("//input[@name='jApplicableFor' and
+		// @type='radio' and @value='2']"))
+		System.out.println(com);
+
 	}
-	
-	
-	
-	
 
 	@Test(priority = 2, enabled = false)
 	public void SKUTableList() {
@@ -46,13 +41,11 @@ public class SKUTest extends BaseTest {
 		// it will check whether will get dashboard header
 		String headerHome = homepage.getDashboardHeader();
 		System.out.println(headerHome);
-	
-	SKU skuList =  page.getInstance(SKU.class).SKUList();
-	
-	String title = skuList.getPageTitle();
-	System.out.println(title);
 
-	
-	
-}
+		SKU skuList = page.getInstance(SKU.class).SKUList();
+
+		String title = skuList.getPageTitle();
+		System.out.println(title);
+
+	}
 }
